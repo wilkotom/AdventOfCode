a0005d4b3abd11abbfea7ce67258f3a76f3efbbe
Tom Wilkinson <tom+github@dentrasssi.net>
2021-01-22 22:47:50 +0000

    Mark Oliver Everett?

--- 10/aoc10.go
+++ 10/aoc10.go
@@ -1,28 +1,27 @@
 package main
+
 import (
-	"strings"
 	"fmt"
 	"io/ioutil"
 	"math"
 	"sort"
+	"strings"
 )
 
 type asteroidLocation struct {
-	X int
-	Y int
-	distance float64
-
+	coordinate [2]int
+	distance   float64
 }
 
-func main() {
+func msain() {
 	inputBytes, err := ioutil.ReadFile("input.txt")
-    if err != nil {
-        panic(err)
+	if err != nil {
+		panic(err)
 	}
 	inputLines := strings.Split(string(inputBytes), "\n")
 	asteroidMap := make([][]int, len(inputLines))
 
-	for i, line := range inputLines{
+	for i, line := range inputLines {
 		asteroidMap[i] = make([]int, len(inputLines[i]))
 		for j, c := range line {
 			if c == 35 {
@@ -31,42 +30,42 @@ func main() {
 		}
 	}
 	// fmt.Println(asteroidMap)
-	mostAsteroids := 0
-	coordinates := [2]int{-1,-1}
-	for i := range inputLines{
+	mostAsteroids := make(map[float64][]asteroidLocation)
+	coordinates := [2]int{-1, -1}
+	for i := range inputLines {
 		for j := range inputLines[i] {
-			if asteroidMap[i][j] == 1 {
-				asteroidsVisible := getVisibleAsteroidCount(i, j, asteroidMap)
-				if len(asteroidsVisible) > mostAsteroids {
-					mostAsteroids = len(asteroidsVisible)
-					coordinates = [2]int{j,i}
+			if asteroidMap[j][i] == 1 {
+				asteroidsVisible := getVisibleAsteroidCount(j, i, asteroidMap)
+				if len(asteroidsVisible) > len(mostAsteroids) {
+					mostAsteroids = asteroidsVisible
+					coordinates = [2]int{j, i}
 				}
 			}
 		}
 
 	}
-	fmt.Println(mostAsteroids, coordinates)
-	asteroids := getVisibleAsteroidCount(coordinates[0], coordinates[1], asteroidMap)
-	for _, bearing := range asteroids {
+	fmt.Println("Most visible Asteroids at:", coordinates, "There are ", len(mostAsteroids), " asteroids visible")
+	// asteroids := getVisibleAsteroidCount(coordinates[0], coordinates[1], asteroidMap)
+	for _, bearing := range mostAsteroids {
 		sort.Slice(bearing, func(i, j int) bool {
 			return bearing[i].distance < bearing[j].distance
-		  })
+		})
 	}
-	angles := make([]float64, 0, len(asteroids))
-	for angle := range asteroids {
+	angles := make([]float64, 0, len(mostAsteroids))
+	for angle := range mostAsteroids {
 		angles = append(angles, angle)
 	}
 	sort.Float64s(angles)
 	count := 0
-	for  count < 41 {
+	for count <= 200 {
 		for _, angle := range angles {
-			fmt.Println(count, angle, asteroids[angle])
-			if len(asteroids[angle]) > 0 {
+			fmt.Println(count, angle, mostAsteroids[angle])
+			if len(mostAsteroids[angle]) > 0 {
 				count++
-				zapped := asteroids[angle][0]
-				asteroidMap[zapped.X][zapped.Y] = count
-				asteroids[angle] = asteroids[angle][1:]
-				if count == 41 {
+				zapped := mostAsteroids[angle][0]
+				asteroidMap[zapped.coordinate[0]][zapped.coordinate[1]] = count
+				mostAsteroids[angle] = mostAsteroids[angle][1:]
+				if count == 200 {
 					fmt.Println(zapped)
 					fmt.Println(asteroidMap)
 					break
@@ -78,20 +77,34 @@ func main() {
 
 func getVisibleAsteroidCount(astX, astY int, asteroidMap [][]int) map[float64][]asteroidLocation {
 	vectors := make(map[float64][]asteroidLocation)
-	for i := range asteroidMap{
-		for j := range asteroidMap[i] {
-			if !(i == astY && j == astX) && asteroidMap[j][i] == 1 {
-				diffY := float64(astY - i)
-				diffX := float64(astX - j)
-				distance := math.Sqrt(diffX * diffX + diffY* diffY )
-				location := asteroidLocation{j,i, distance}
-				vector := math.Atan2(-diffY,diffX)
-				if vector < 0 {
-					vector = vector + 2 * math.Pi
+	for y := range asteroidMap {
+		for x := range asteroidMap[y] {
+			if !(x == astX && y == astY) && asteroidMap[x][y] == 1 {
+				diffX := float64(x - astX)
+				diffY := float64(y - astY)
+				distance := math.Sqrt(diffX*diffX + diffY*diffY)
+				location := asteroidLocation{[2]int{x, y}, distance}
+				vector := math.Atan2(diffX, diffY) + math.Pi
+				if vector >= 2*math.Pi {
+					vector = vector - 2*math.Pi
 				}
-				vector = vector + 0.5 * math.Pi
-				// fmt.Println("Can see asteroid at ", i, j, math.Atan2(diffY, diffX))
-				vectors[vector] = append(vectors[vector],location)
+				// if vector <= math.Pi && vector >= 0 {
+				// 	vector = math.Abs(vector - math.Pi/2)
+				// } else if vector < 0 {
+				// 	vector = math.Abs(vector) + math.Pi/2
+				// } else {
+				// 	vector = math.Pi*1.5 - vector
+				// }
+
+				// 	vector = vector + 2*math.Pi
+				// } else if vector >= 2*math.Pi {
+				// 	vector = 2*math.Pi - vector
+				// }
+				// vector = 2*math.Pi - vector
+				// if vector >= 2*math.Pi {
+				// 	vector = 2*math.Pi - vector
+				// }
+				vectors[vector] = append(vectors[vector], location)
 			}
 		}
 	}

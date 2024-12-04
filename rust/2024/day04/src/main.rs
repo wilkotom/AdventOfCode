798af58cb200121e0156db81889c548bd7de3667
Tom Wilkinson <tom.github@dentrasssi.net>
2024-12-04 20:06:04 +0000

    Using Grid and checked_neighbour

--- rust/2024/day04/src/main.rs
+++ rust/2024/day04/src/main.rs
@@ -1,9 +1,9 @@
-use std::{collections::HashMap, error::Error, result};
-use aochelpers::{get_daily_input, Coordinate, Direction};
+use std::{error::Error, usize};
+use aochelpers::{get_daily_input, Coordinate, Direction, Grid};
 
 fn main() -> Result<(), Box<dyn Error>>{
     let data = get_daily_input(04,2024)?;
-    let grid: HashMap<Coordinate<i32>, char> = parse_data(&data);
+    let grid = parse_data(&data);
     println!("Part 1: {}", part1(&grid));
     println!("Part 2: {}", part2(&grid));
 
@@ -11,16 +11,19 @@ fn main() -> Result<(), Box<dyn Error>>{
 
 }
 
-fn part1(grid: &HashMap<Coordinate<i32>, char>) -> usize {
-
+fn part1(grid: &Grid<char>) -> usize {
     let mut score = 0;
-    for point in grid.keys() {
+    for point in grid.keys::<usize>() {
         for direction in [Direction::North, Direction::NorthEast, Direction::East, Direction::SouthEast] {
-            let mut location = *point;
+            let mut location = point;
             let mut readout = String::new();
             for _ in 0..4 {
-                readout.push(*grid.get(&location).unwrap_or(&'#'));
-                location = location.neighbour(direction)
+                readout.push(grid.get(&location).unwrap_or('#'));
+                if let Some(next_location) = location.checked_neighbour(direction) {
+                    location = next_location;
+                } else {
+                    break;
+                }
             }
             if readout == "XMAS" || readout == "SAMX" {
                 score +=1;
@@ -30,22 +33,22 @@ fn part1(grid: &HashMap<Coordinate<i32>, char>) -> usize {
     score
 }
 
-fn part2(grid: &HashMap<Coordinate<i32>, char>) -> usize {
+fn part2(grid: &Grid<char>) -> usize {
 
     let mut score = 0;
-    for (point, c) in grid{
-        if c == &'A' {
+    for (point, c) in grid.iter::<usize>() {
+        if c == 'A' {
             let mut diagonal = String::new();
-            diagonal.push(*grid.get(&point.neighbour(Direction::NorthWest)).unwrap_or(&'#'));
+            diagonal.push(grid.get(&point.checked_neighbour(Direction::NorthWest).unwrap_or(Coordinate{x:usize::MAX, y:usize::MAX})).unwrap_or('#'));
             diagonal.push('A');
-            diagonal.push(*grid.get(&point.neighbour(Direction::SouthEast)).unwrap_or(&'#'));
+            diagonal.push(grid.get(&point.checked_neighbour(Direction::SouthEast).unwrap_or(Coordinate{x:usize::MAX, y:usize::MAX})).unwrap_or('#'));
             if ! ["MAS", "SAM"].contains(&&diagonal[..]) {
                 continue;
             }
             diagonal.clear();
-            diagonal.push(*grid.get(&point.neighbour(Direction::SouthWest)).unwrap_or(&'#'));
+            diagonal.push(grid.get(&point.checked_neighbour(Direction::NorthEast).unwrap_or(Coordinate{x:usize::MAX, y:usize::MAX})).unwrap_or('#'));
             diagonal.push('A');
-            diagonal.push(*grid.get(&point.neighbour(Direction::NorthEast)).unwrap_or(&'#'));
+            diagonal.push(grid.get(&point.checked_neighbour(Direction::SouthWest).unwrap_or(Coordinate{x:usize::MAX, y:usize::MAX})).unwrap_or('#'));
 
             if ["MAS", "SAM"].contains(&&diagonal[..]) {
                 score+=1;
@@ -55,11 +58,11 @@ fn part2(grid: &HashMap<Coordinate<i32>, char>) -> usize {
     score
 }
 
-fn parse_data(data: &str) -> HashMap<Coordinate<i32>, char>{
-    let mut grid = HashMap::new();
+fn parse_data(data: &str) -> Grid<char>{
+    let mut grid = Grid::new();
     for (y,line) in data.lines().enumerate() {
         for (x,c) in line.chars().enumerate(){
-            grid.insert(Coordinate{x: x as i32,y: y as i32}, c);
+            grid.insert(Coordinate{x,y}, c);
         }
     }
     grid
@@ -81,14 +84,14 @@ MAMMMXMMMM
 MXMXAXMASX";
     #[test]
     fn test_part1() {
-        let grid: HashMap<Coordinate<i32>, char> = parse_data(TESTDATA);
+        let grid = parse_data(TESTDATA);
         assert_eq!(part1(&grid),18);
         
     }
 
     #[test]
     fn test_part2() {
-        let grid: HashMap<Coordinate<i32>, char> = parse_data(TESTDATA);
+        let grid = parse_data(TESTDATA);
         assert_eq!(part2(&grid),9);
 
     }

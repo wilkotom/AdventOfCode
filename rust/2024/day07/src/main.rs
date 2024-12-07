9bddf37376147e84f853e231c6bde17ea9266472
Tom Wilkinson <tom.github@dentrasssi.net>
2024-12-07 09:08:48 +0000

    Somewhat less Long-running calibrations

--- rust/2024/day07/src/main.rs
+++ rust/2024/day07/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, error::Error};
+use std::error::Error;
 use aochelpers::get_daily_input;
 
 #[derive(Debug,Clone)]
@@ -10,53 +10,48 @@ struct CalibrationState {
 fn main() -> Result<(), Box<dyn Error>>{
     let data = get_daily_input(7,2024)?;
     let parsed: Vec<CalibrationState> = parse_data(&data);
-    println!("Part 1: {}", solve(parsed, false));
-    let parsed: Vec<CalibrationState> = parse_data(&data);
-    println!("Part 2: {}", solve(parsed, true));
-
+    println!("Part 1: {}", solve(&parsed, false));
+    println!("Part 2: {}", solve(&parsed, true));
     Ok(())
 }
 
-fn solve(mut states: Vec<CalibrationState>, part2: bool) -> i64{
-    let mut targets_met = HashSet::new();
-
-    while let Some(mut state) = states.pop() {
-        if state.numbers.len() == 1 {
-            if state.numbers.pop().unwrap() == state.target {
-                targets_met.insert(state.target);
+fn solve(states: &Vec<CalibrationState>, part2: bool) -> i64{
+    let mut answer = 0;
+    let mut unprocessed = Vec::new();
+    'outer: for state in states {
+        unprocessed.clear();
+        unprocessed.push((0,0));
+        while let Some((accumulator, pointer)) = unprocessed.pop(){
+            if pointer == state.numbers.len() {
+                if accumulator == state.target {
+                    answer += state.target;
+                    continue 'outer;
+                }
+                continue;
             }
-            continue;
-        }
-        let left = state.numbers.pop().unwrap();
-        let right = state.numbers.pop().unwrap();
-
-        let mut mul_state = state.clone();
-        mul_state.numbers.push(left*right);
-        states.push(mul_state);
-
-        if part2 {
-            let mut cat_state = state.clone();
-            let mut cat_shift = 1;
-            while ((cat_shift * left) + right) % cat_shift != right {
-                cat_shift *=10
+            if accumulator >= state.target {
+                continue;
             }
-            cat_state.numbers.push((cat_shift * left)+right);
-            states.push(cat_state);
+            unprocessed.push((accumulator + state.numbers[pointer], pointer+1));
+            unprocessed.push((accumulator * state.numbers[pointer], pointer+1));
+            if part2 {
+                let mut cat_shift = 10;
+                while ((cat_shift * accumulator) + state.numbers[pointer]) % cat_shift != state.numbers[pointer] {
+                    cat_shift *=10
+                }
+                unprocessed.push((accumulator * cat_shift + state.numbers[pointer], pointer+1));
+            }
         }
-
-        state.numbers.push(left+right);
-        states.push(state);
     }
-    targets_met.iter().sum()
+    answer
 }
-
 
 fn parse_data (data: &str) -> Vec<CalibrationState>{
     let mut results = Vec::new();
     for line in data.lines() {
         let split = line.chars().position(|c |c ==':').unwrap();
         let target = line[..split].parse().unwrap();
-        let numbers = line[split+1..].split_ascii_whitespace().map(|v| v.parse().unwrap()).rev().collect::<Vec<_>>();
+        let numbers = line[split+1..].split_ascii_whitespace().map(|v| v.parse().unwrap()).collect::<Vec<_>>();
         results.push(CalibrationState{
             target,
             numbers,
@@ -71,7 +66,7 @@ fn parse_data (data: &str) -> Vec<CalibrationState>{
 mod tests {
     use super::*;
 
-    const TESTDATA: &str = "190: 10 19
+    const TESTDATA: &str = "190: 10 19 
 3267: 81 40 27
 83: 17 5
 156: 15 6
@@ -85,12 +80,12 @@ mod tests {
     fn test_part1() {
         let parsed: Vec<CalibrationState> = parse_data(TESTDATA);
         println!("{:?}", parsed);
-        assert_eq!(solve(parsed, false), 3749)
+        assert_eq!(solve(&parsed, false), 3749)
     }
 
     #[test]
     fn test_part2() {
         let parsed: Vec<CalibrationState> = parse_data(TESTDATA);
-        assert_eq!(solve(parsed, true), 11387)
+        assert_eq!(solve(&parsed, true), 11387)
     }
 }

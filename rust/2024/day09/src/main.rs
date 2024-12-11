81d7c82665bfdd90ed5865d7acc4b47ca566c763
Tom Wilkinson <tom.github@dentrasssi.net>
2024-12-11 19:00:15 +0000

    New, fresh, optimised

--- rust/2024/day09/src/main.rs
+++ rust/2024/day09/src/main.rs
@@ -1,17 +1,18 @@
-use std::{collections::VecDeque, error::Error, path::is_separator};
-use aochelpers::get_daily_input;
+use core::alloc;
+use std::{collections::{BinaryHeap, VecDeque}, error::Error};
+use aochelpers::{get_daily_input, ScoredItem};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 enum BlockType {
     Filled(usize), 
     Empty
 }
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 
 struct BlockAllocation{ 
     length: usize,
-    block_type: BlockType
-
+    block_type: BlockType,
+    position: usize
 }
 fn main() -> Result<(), Box<dyn Error>>{
     let data = get_daily_input(9,2024)?;
@@ -35,7 +36,7 @@ fn defragment(mut disk_map: VecDeque<BlockAllocation>) -> usize {
                     defragmented_disk.push(next_file);
                     available_blocks -= next_file.length;
                 } else {
-                    let next_fragment = BlockAllocation{length: available_blocks, block_type: next_file.block_type};
+                    let next_fragment = BlockAllocation{length: available_blocks, block_type: next_file.block_type, position: 0};
                     defragmented_disk.push(next_fragment);
                     next_file.length -=available_blocks;
                     available_blocks = 0;
@@ -52,7 +53,7 @@ fn defragment(mut disk_map: VecDeque<BlockAllocation>) -> usize {
                                     defragmented_disk.push(next_file);
                                     next_file.length = 0;
                                 } else {
-                                    let next_fragment = BlockAllocation{length: b.length, block_type: next_file.block_type};
+                                    let next_fragment = BlockAllocation{length: b.length, block_type: next_file.block_type, position: 0};
                                     defragmented_disk.push(next_fragment);
                                     next_file.length -= b.length;
                                     available_blocks -= b.length;
@@ -73,46 +74,38 @@ fn defragment(mut disk_map: VecDeque<BlockAllocation>) -> usize {
 }
 
 fn defragment2(mut disk_map: Vec<BlockAllocation>) -> usize {
-
-    let mut unmovable = Vec::new();
-    while let Some(next_file) = disk_map.pop() {
-        match next_file.block_type {
-            BlockType::Filled(n) => {
-                let mut next_map: Vec<BlockAllocation> = Vec::new();
-                let mut placed = false;
-                for mut entry in disk_map.iter().copied() {
-                    match entry.block_type {
-                        BlockType::Empty if !placed => {
-                            if entry.length >= next_file.length{
-                                next_map.push(next_file);
-                                unmovable.push(BlockAllocation{block_type: BlockType::Empty, length: next_file.length});
-                                entry.length -= next_file.length;
-                                placed = true;
-                            }
-                            if entry.length != 0 {
-                                next_map.push(entry); 
-                            }
-                        },
-                        _ => {
-                            next_map.push(entry);
-                        },
-                    }
-                }
-                if !placed {
-                    unmovable.push(next_file);
-                } 
-
-                disk_map = consolidate_blocks(next_map);
-            },
-            BlockType::Empty => {
-                unmovable.push(next_file);
-            },
+    let mut unused_block_fragments =  vec![BinaryHeap::new(); 10];
+    for allocation in &disk_map {
+        if allocation.block_type == BlockType::Empty {
+            unused_block_fragments[allocation.length].push(ScoredItem{cost: allocation.position, item: *allocation});
         }
     }
-    unmovable.reverse();
-    score_defrag(&unmovable)
-}
+    let mut total = 0;
+    while let Some(mut allocation) = disk_map.pop() {
+        if let BlockType::Filled(id) = allocation.block_type {
+        let mut length_selection = None;
+        let mut best_position = allocation.position;
+        for free_length in allocation.length..=9 {
+            if let Some(potential_position) = unused_block_fragments[free_length].peek() {
+                if best_position > potential_position.item.position {
+                    best_position = potential_position.item.position;
+                    length_selection = Some(free_length);
+                }
+            }
+        } 
+        if let Some(length) = length_selection {
+            let mut unallocated: BlockAllocation =  unused_block_fragments[length].pop().unwrap().item;
+            allocation.position = unallocated.position;
+            unallocated.length -= allocation.length;
+            unallocated.position += allocation.length;
+            unused_block_fragments[unallocated.length].push(ScoredItem{cost: unallocated.position, item: unallocated});
 
+        }
+        total += ((allocation.length * allocation.position) + ((allocation.length-1) * allocation.length) /2) * id
+        }
+    }
+    total
+}
 fn score_defrag(defragmented_disk: &Vec<BlockAllocation>) -> usize{
     let mut position = 0;
     let mut result = 0;
@@ -129,32 +122,20 @@ fn score_defrag(defragmented_disk: &Vec<BlockAllocation>) -> usize{
 
     result
 }
-
-fn consolidate_blocks(filesystem: Vec<BlockAllocation>) -> Vec<BlockAllocation>{
-    let mut next_fs = Vec::new();
-    let mut last_block = BlockAllocation{block_type: BlockType::Empty, length: 0};
-    for item in filesystem {
-        if item.block_type == BlockType::Empty && last_block.block_type == BlockType::Empty {
-            last_block.length += item.length;
-        } else {
-            next_fs.push(last_block);
-            last_block = item;
-        }
-    }
-    next_fs.push(last_block);
-    next_fs
-}
  
 fn parse_data(data: &str) -> Vec<BlockAllocation> {
     let mut result = Vec::new();
     let mut current_block_id = 0;
+    let mut position = 0;
     for (i, c) in data.chars().enumerate() {
+        let length = c.to_digit(10).unwrap() as usize;
         if i %2 == 0 {
-            result.push(BlockAllocation{length: c.to_digit(10).unwrap() as usize, block_type: BlockType::Filled(current_block_id)});
+            result.push(BlockAllocation{length, block_type: BlockType::Filled(current_block_id), position});
             current_block_id +=1;
         } else {
-            result.push(BlockAllocation{length: c.to_digit(10).unwrap() as usize, block_type: BlockType::Empty});
+            result.push(BlockAllocation{length, block_type: BlockType::Empty, position});
         }
+        position += length;
     }
     result
 }
@@ -166,34 +147,32 @@ mod tests {
 
     #[test]
     fn test_parser() {
-        let expected = vec![BlockAllocation{length:1, block_type:BlockType::Filled(0)},
-                                                                    BlockAllocation{length:2, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:3, block_type:BlockType::Filled(1)},
-                                                                    BlockAllocation{length:4, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:5, block_type:BlockType::Filled(2)}];
+        let expected = vec![BlockAllocation{length:1, block_type:BlockType::Filled(0), position: 0},
+                                                                    BlockAllocation{length:2, block_type:BlockType::Empty, position:1},
+                                                                    BlockAllocation{length:3, block_type:BlockType::Filled(1), position:3},
+                                                                    BlockAllocation{length:4, block_type:BlockType::Empty, position: 6},
+                                                                    BlockAllocation{length:5, block_type:BlockType::Filled(2), position: 10}];
         assert_eq!(parse_data("12345"), expected);
 
-        let expected = vec![BlockAllocation{length:2, block_type:BlockType::Filled(0)},
-                                                                    BlockAllocation{length:3, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:3, block_type:BlockType::Filled(1)},
-                                                                    BlockAllocation{length:3, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:1, block_type:BlockType::Filled(2)},
-                                                                    BlockAllocation{length:3, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:3, block_type:BlockType::Filled(3)},
-                                                                    BlockAllocation{length:1, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:2, block_type:BlockType::Filled(4)},
-                                                                    BlockAllocation{length:1, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:4, block_type:BlockType::Filled(5)},
-                                                                    BlockAllocation{length:1, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:4, block_type:BlockType::Filled(6)},
-                                                                    BlockAllocation{length:1, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:3, block_type:BlockType::Filled(7)},
-                                                                    BlockAllocation{length:1, block_type:BlockType::Empty},
-                                                                    BlockAllocation{length:4, block_type:BlockType::Filled(8)},
-                                                                    BlockAllocation{length:0, block_type:BlockType::Empty},
-
-                                                                    BlockAllocation{length:2, block_type:BlockType::Filled(9)},
-                                                                    ];
+        let expected = vec! [BlockAllocation { length: 2, block_type: BlockType::Filled(0), position: 0 }, 
+            BlockAllocation { length: 3, block_type: BlockType::Empty, position: 2 }, 
+            BlockAllocation { length: 3, block_type: BlockType::Filled(1), position: 5 }, 
+            BlockAllocation { length: 3, block_type: BlockType::Empty, position: 8 }, 
+            BlockAllocation { length: 1, block_type: BlockType::Filled(2), position: 11 }, 
+            BlockAllocation { length: 3, block_type: BlockType::Empty, position: 12 }, 
+             BlockAllocation { length: 3, block_type: BlockType::Filled(3), position: 15 }, 
+             BlockAllocation { length: 1, block_type: BlockType::Empty, position: 18 }, 
+             BlockAllocation { length: 2, block_type: BlockType::Filled(4), position: 19 }, 
+             BlockAllocation { length: 1, block_type: BlockType::Empty, position: 21 }, 
+             BlockAllocation { length: 4, block_type: BlockType::Filled(5), position: 22 }, 
+             BlockAllocation { length: 1, block_type: BlockType::Empty, position: 26 }, 
+             BlockAllocation { length: 4, block_type: BlockType::Filled(6), position: 27 }, 
+             BlockAllocation { length: 1, block_type: BlockType::Empty, position: 31 }, 
+             BlockAllocation { length: 3, block_type: BlockType::Filled(7), position: 32 }, 
+             BlockAllocation { length: 1, block_type: BlockType::Empty, position: 35 }, 
+             BlockAllocation { length: 4, block_type: BlockType::Filled(8), position: 36 }, 
+             BlockAllocation { length: 0, block_type: BlockType::Empty, position: 40 }, 
+             BlockAllocation { length: 2, block_type: BlockType::Filled(9), position: 40 }];
         assert_eq!(parse_data("2333133121414131402"), expected);
 
     }

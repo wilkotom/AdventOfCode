a2d343be3eacf8e7236d81d851e5ac2fb7a0aad0
Tom Wilkinson <tom.github@dentrasssi.net>
2024-12-12 22:46:17 +0000

    Thrice widdershins around the block?

--- rust/2024/day12/src/main.rs
+++ rust/2024/day12/src/main.rs
@@ -1,32 +1,45 @@
 use std::{collections::{HashMap, HashSet}, error::Error};
 use aochelpers::{get_daily_input, Coordinate, Direction, Grid};
+use std::time::{Duration, Instant};
 
 fn main() -> Result<(), Box<dyn Error>>{
     let data = get_daily_input(12,2024)?;
     let garden = parse_data(&data);
+    let start: Instant = Instant::now();
     println!("Part 1: {}", solve(&garden, false));
     println!("Part 2: {}", solve(&garden, true));
-
+    println!("Elapsed: {}ms", start.elapsed().as_millis());
     Ok(())
 }
 
-fn solve(garden: &HashMap<Coordinate<i32>,char>, part2: bool) -> usize {
+fn parse_data(data: &str) -> Grid<char> {
+    let mut grid = Grid::new();
+    for (y, line) in data.lines().enumerate() {
+        for (x,c) in line.chars().enumerate() {
+            grid.insert(Coordinate{x, y}, c);
+        }    
+    }
+    grid
+}
+
+
+fn solve(garden: &Grid<char>, part2: bool) -> usize {
     let mut result = 0;
-    let mut visited = HashSet::new();
+    let mut visited: HashSet<Coordinate<usize>> = HashSet::new();
     for (loc, label) in garden.iter() {
-        if visited.contains(loc) {
+        if visited.contains(&loc) {
             continue;
         }
         let mut poly = HashSet::new();
-        poly.insert(*loc);
+        poly.insert(loc);
         let mut unconsidered = Vec::new();
-        loc.neighbours().into_iter().filter(|n| ! visited.contains(n)).for_each(|n| unconsidered.push(n));
+        loc.checked_neighbours().filter(|n| ! visited.contains(n)).for_each(|n| unconsidered.push(n));
         while let Some (neighbour) = unconsidered.pop() {
             if visited.contains(&neighbour) { continue;}
             if garden.get(&neighbour) == Some(label) {
                 visited.insert(neighbour);
                 poly.insert(neighbour);
-                neighbour.neighbours().iter().filter(|n| ! visited.contains(n)).for_each(|n| unconsidered.push(*n));
+                neighbour.checked_neighbours().filter(|n| ! visited.contains(n)).for_each(|n| unconsidered.push(n));
             }
         }
         result += if part2 {get_sides(&poly)} else {get_perimeter(&poly)} * poly.len();
@@ -34,66 +47,46 @@ fn solve(garden: &HashMap<Coordinate<i32>,char>, part2: bool) -> usize {
     result
 }
 
-fn get_perimeter( field: &HashSet<Coordinate<i32>>) -> usize {
+fn get_perimeter( field: &HashSet<Coordinate<usize>>) -> usize {
     let mut total = 0;
     for square in field.iter() {
-        total += square.neighbours().into_iter().filter(|n| !field.contains(n)).count();
+        total += 4 - square.checked_neighbours().filter(|n| field.contains(&n)).count();
     }
     total
 }
 
-fn get_sides( field: &HashSet<Coordinate<i32>>) -> usize {
+fn get_sides( field: &HashSet<Coordinate<usize>>) -> usize {
     let mut total = 0;
 
     // Number of edges is the same as the number of corners. Count the corners.
     for square in field.iter() {
         // External corners
-        let unattached_count = square.neighbours().into_iter().filter(|n| !field.contains(n)).count();
-        total += match unattached_count {
-            4 => 4,
-            3 => 2,
-            2 => if (field.contains(&square.neighbour(Direction::North)) && field.contains(&square.neighbour(Direction::South))) ||
-                    (field.contains(&square.neighbour(Direction::East)) && field.contains(&square.neighbour(Direction::West))) { 0 } else {1},
+        let attached_count = square.checked_neighbours().filter(|n| field.contains(n)).count();
+        total += match attached_count {
+            0 => 4,
+            1 => 2,
+            2 => if (square.checked_neighbour(Direction::North).is_some() && field.contains(&square.neighbour(Direction::North)) &&
+                        square.checked_neighbour(Direction::South).is_some() && field.contains(&square.neighbour(Direction::South))) ||
+                        (square.checked_neighbour(Direction::East).is_some() && field.contains(&square.neighbour(Direction::East)) &&
+                        square.checked_neighbour(Direction::West).is_some() && field.contains(&square.neighbour(Direction::West)))  { 0 } else {1},
             _ => 0,
         };
-
         // Inner corners. If the diagonal neighbour is missing but the vertical and horizontal ones are not.
 
-        if !field.contains(&square.neighbour(Direction::NorthEast)) && 
-                field.contains(&square.neighbour(Direction::North))  && 
-                field.contains(&square.neighbour(Direction::East)) {
-            total += 1;
-        }
-        if !field.contains(&square.neighbour(Direction::SouthEast)) && 
-                field.contains(&square.neighbour(Direction::South))  && 
-                field.contains(&square.neighbour(Direction::East)) {
-            total += 1;
-        }
-        if !field.contains(&square.neighbour(Direction::NorthWest)) && 
-                field.contains(&square.neighbour(Direction::North))  && 
-                field.contains(&square.neighbour(Direction::West)) {
-            total += 1;
-        }
-        if !field.contains(&square.neighbour(Direction::SouthWest)) && 
-                field.contains(&square.neighbour(Direction::South))  && 
-                field.contains(&square.neighbour(Direction::West)) {
-            total += 1;
+        for (widdershins, middle, turnwise) in [
+                    (Direction::North, Direction::NorthEast, Direction::East), 
+                    (Direction::East, Direction::SouthEast, Direction::South), 
+                    (Direction::South, Direction::SouthWest, Direction::West), 
+                    (Direction::West, Direction::NorthWest, Direction::North)] { 
+            if ( square.checked_neighbour(middle).is_none() || !field.contains(&square.neighbour(middle))) && 
+                    square.checked_neighbour(widdershins).is_some() && field.contains(&square.neighbour(widdershins))  && 
+                    square.checked_neighbour(turnwise).is_some() && field.contains(&square.neighbour(turnwise))  {
+                total += 1;
+            }
         }
     }
     total
 }
-
-fn parse_data(data: &str) -> HashMap<Coordinate<i32>, char> {
-    let mut grid = HashMap::new();
-    for (y, line) in data.lines().enumerate() {
-        for (x,c) in line.chars().enumerate() {
-            grid.insert(Coordinate{x: x as i32,y: y as i32}, c);
-        }    
-    }
-    grid
-}
-
-
 #[cfg(test)]
 mod tests {
 

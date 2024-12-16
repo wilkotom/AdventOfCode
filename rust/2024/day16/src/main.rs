bbf691b0f4c00427a22528bb004a6bfc61389a9a
Tom Wilkinson <tom.github@dentrasssi.net>
2024-12-16 21:09:18 +0000

    The race winner is... not my previous algorithm.

--- rust/2024/day16/src/main.rs
+++ rust/2024/day16/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{BinaryHeap, HashMap, HashSet}, error::Error, os::macos::raw::stat};
+use std::{collections::{BinaryHeap, HashMap, HashSet}, error::Error};
 use aochelpers::{get_daily_input, Coordinate, Direction, Grid, ScoredItem};
 
 #[derive(Debug,Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -15,137 +15,62 @@ struct Position {
     facing: Direction
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct PositionWithPathTaken {
-    location: Coordinate<usize>,
-    facing: Direction,
-    visited: Vec<(usize,Position)>
-}
-
 fn main() -> Result<(), Box<dyn Error>>{
     let data = get_daily_input(16,2024)?;
     let grid = parse_data(&data);
     let (part1, part2) = solve(&grid);
     println!("Part 1: {}", part1);
-    println!("Part 2: {:?}", part2);
+    println!("Part 2: {}", part2);
     Ok(())
 }
 
-fn solve(grid: &Grid<Square>) -> (usize,usize) {
-
+fn flood_fill(grid: &Grid<Square>, start_location: Position) -> HashMap<Position, usize> {
     let mut visited = HashMap::new();
     let mut unvisited = BinaryHeap::new();
-    let start_location = grid.iter::<usize>().filter(|(_,s)| *s == Square::Start).map(|(c,_)|c).next().expect("There is no Start line");
-    unvisited.push(ScoredItem{cost: 0, item: PositionWithPathTaken{ location: start_location, facing: Direction::East, visited: Vec::new()}});
-    let mut best_path_score = usize::MAX;
-    let mut best_paths = HashMap::new();
-    while let Some(mut state) = unvisited.pop() {
-        let cacheable_state = Position{location: state.item.location, facing: state.item.facing};
-        if visited.contains_key(&cacheable_state) {
+    unvisited.push(ScoredItem{cost: 0, item: start_location});
+    while let Some(state) = unvisited.pop() {
+        if visited.contains_key(&state.item) {
             continue;
         }
-        visited.insert(cacheable_state, state.cost);
-        state.item.visited.push((state.cost, cacheable_state));
+        visited.insert(state.item, state.cost);
         match state.item.facing {
-            Direction::North => {
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::East, visited: state.item.visited.clone()}});
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::West, visited: state.item.visited.clone()}});
+            Direction::North | Direction::South => {
+                unvisited.push(ScoredItem{cost: state.cost + 1000, item: Position{ location: state.item.location, facing:Direction::East}});
+                unvisited.push(ScoredItem{cost: state.cost + 1000, item: Position{ location: state.item.location, facing:Direction::West}});
                 },
-            Direction::East => {
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::North, visited: state.item.visited.clone()}});
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::South, visited: state.item.visited.clone()}});
+            Direction::East | Direction::West => {
+                unvisited.push(ScoredItem{cost: state.cost + 1000, item: Position{ location: state.item.location, facing:Direction::North}});
+                unvisited.push(ScoredItem{cost: state.cost + 1000, item: Position{ location: state.item.location, facing:Direction::South}});
             },
-            Direction::South => {
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::West, visited: state.item.visited.clone()}});
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::East, visited: state.item.visited.clone()}});
-
-            },
-            Direction::West => {
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::North, visited: state.item.visited.clone()}});
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::South, visited: state.item.visited.clone()}});
-            }
             _=> unimplemented!()
         }
-        let next_square =  grid.get(&state.item.location.neighbour(state.item.facing));
-        match next_square {
-            Some(Square::Empty) | Some(Square::Start) =>{
-                unvisited.push(ScoredItem{cost: state.cost + 1, item: PositionWithPathTaken{ location: state.item.location.neighbour(state.item.facing), facing:state.item.facing, visited: state.item.visited}});
+        match grid.get(&state.item.location.neighbour(state.item.facing)) {
+            Some(Square::Empty) |  Some(Square::End) | Some(Square::Start) =>{
+                unvisited.push(ScoredItem{cost: state.cost + 1, item: Position{ location: state.item.location.neighbour(state.item.facing), facing:state.item.facing}});
             }
-            Some(Square::End) => {
-                best_path_score = best_path_score.min(state.cost +1);
-                if state.cost +1 == best_path_score {
-                    for (c,l) in state.item.visited {
-                        best_paths.insert(l, c);
-                    }
-                }
-            }
-            Some(Square::Wall) => {
+            Some(Square::Wall)  => {
             }
             _ => unimplemented!()
         }
-
     }
-    unvisited.clear();
-    unvisited.push(ScoredItem{cost: 0, item: PositionWithPathTaken{ location: start_location, facing: Direction::East, visited: Vec::new()}});
-    while let Some(mut state) = unvisited.pop() {
-        if state.cost > best_path_score {
-            continue;
-        }
-        let cacheable_state = Position{location: state.item.location, facing: state.item.facing};
-        if let Some(best_score) = best_paths.get(&cacheable_state) {
-            match best_score.cmp(&state.cost) {
-                std::cmp::Ordering::Less => {
-                    continue;
-                },
-                std::cmp::Ordering::Equal => {
-                    for (c, p) in state.item.visited.iter() {
-                        best_paths.insert(*p, *c);
-                    }
-                },
-                std::cmp::Ordering::Greater => {unimplemented!()},
-            }
-        } else if let Some(possible_cost) = visited.get(&cacheable_state) {
-            if *possible_cost < state.cost {
-             continue;
-            }
-        }
-        if !best_paths.contains_key(&cacheable_state) {
-            visited.insert(cacheable_state, state.cost);
-            state.item.visited.push((state.cost, cacheable_state));
-        }
-        match state.item.facing {
-            Direction::North => {
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::East, visited: state.item.visited.clone()}});
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::West, visited: state.item.visited.clone()}});
-                },
-            Direction::East => {
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::North, visited: state.item.visited.clone()}});
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::South, visited: state.item.visited.clone()}});
-            },
-            Direction::South => {
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::West, visited: state.item.visited.clone()}});
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::East, visited: state.item.visited.clone()}});
+    visited
+}
 
-            },
-            Direction::West => {
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::North, visited: state.item.visited.clone()}});
-                unvisited.push(ScoredItem{cost: state.cost + 1000, item: PositionWithPathTaken{ location: state.item.location, facing:Direction::South, visited: state.item.visited.clone()}});
+fn solve(grid: &Grid<Square>) -> (usize,usize) {
+    let start = grid.iter::<usize>().find(|(_,s)| *s == Square::Start).map(|(c,_)| c).expect("No starting point");
+    let end = grid.iter::<usize>().find(|(_,s)| *s == Square::End).map(|(c,_)| c).expect("No ending point");
+    let start_map = flood_fill(grid, Position{location: start, facing: Direction::East});
+    let end_map = flood_fill(grid, Position{location: end, facing: Direction::South});
+    let optimum_distance = *start_map.get(&Position{location: end, facing: Direction::North}).unwrap_or(&0);    
+    let mut good_squares = HashSet::new();
+    for (start_dir, end_dir) in [(Direction::North, Direction::South), (Direction::East, Direction::West), (Direction::South, Direction::North), (Direction::West, Direction::East)] {
+        for square in grid.keys::<usize>() {
+            if *start_map.get(&Position{location: square, facing: start_dir}).unwrap_or(&0) + *end_map.get(&Position{location: square, facing: end_dir}).unwrap_or(&0) == optimum_distance  {
+                good_squares.insert(square);
             }
-            _=> unimplemented!()
         }
-        let next_square =  grid.get(&state.item.location.neighbour(state.item.facing));
-        match next_square {
-            Some(Square::Empty) | Some(Square::Start) =>{
-                unvisited.push(ScoredItem{cost: state.cost + 1, item: PositionWithPathTaken{ location: state.item.location.neighbour(state.item.facing), facing:state.item.facing, visited: state.item.visited}});
-            }
-            Some(Square::End) | Some(Square::Wall) => {
-            }
-            _ => unimplemented!()
-        }
-
-
     }
-    (best_path_score, best_paths.keys().map(|p| p.location).collect::<HashSet<_>>().len() +1)
+    (*start_map.get(&Position{location: end, facing: Direction::North}).unwrap_or(&0), good_squares.len())
 }
 
 fn parse_data(input: &str) -> Grid<Square> {
@@ -215,4 +140,5 @@ mod tests {
         let grid = parse_data(TEST2);
         assert_eq!(solve(&grid), (11048, 64));
     }
+
 }

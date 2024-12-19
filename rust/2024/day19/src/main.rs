4b990eba29a4cb584fddb399b30a5824b9fce406
Tom Wilkinson <tom.github@dentrasssi.net>
2024-12-19 19:53:42 +0000

    Re-weaving the towel: Small optimisations / refactoring

--- rust/2024/day19/src/main.rs
+++ rust/2024/day19/src/main.rs
@@ -4,36 +4,32 @@ use aochelpers::get_daily_input;
 fn main() -> Result<(), Box<dyn Error>>{
     let data = get_daily_input(19,2024)?;
     let (valid, designs) = parse_data(&data);
-    println!("Part 1: {}", part1(&designs, &valid));
-    println!("Part 2: {}", part2(&designs, &valid));
+    let (part1, part2) = solve(&designs, &valid);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
     Ok(())
 }
 
-fn part1(designs: &Vec<&str>, valid: &Vec<&str>) -> usize {
+fn solve(designs: &Vec<&str>, valid: &[&str]) -> (usize, usize) {
     let mut already_computed = HashMap::new();
     let max_pattern_len = valid.iter().map(|v| v.len()).max().unwrap();
-    designs.iter().filter(|p| count_valid_pattern(p, valid, &mut already_computed, max_pattern_len) > 0).count()
+    let part1 = designs.iter().filter(|p| count_valid_patterns(p, valid, &mut already_computed, max_pattern_len) > 0).count();
+    // As we've built the cache, Part 2 is basically free
+    let part2 = designs.iter().map(|p| count_valid_patterns(p, valid, &mut already_computed, max_pattern_len)).sum();
+    (part1, part2)
 }
 
-fn part2(designs: &Vec<&str>, valid: &Vec<&str>) -> usize {
-    let mut already_computed = HashMap::new();
-    let max_pattern_len = valid.iter().map(|v| v.len()).max().unwrap();
-    designs.iter().map(|p| count_valid_pattern(p, valid, &mut already_computed, max_pattern_len)).sum()
-}
-
-
-fn count_valid_pattern<'a>(pattern: &'a str, valid: &Vec<&'a str>, already_computed: &mut HashMap<&'a str, usize>, max_len: usize) -> usize {
+fn count_valid_patterns<'a>(pattern: &'a str, valid: &[&'a str], already_computed: &mut HashMap<&'a str, usize>, max_len: usize) -> usize {
     let mut combinations = 0;
-    if already_computed.contains_key(pattern) {
-        return *already_computed.get(pattern).unwrap();
+    if let Some(result) = already_computed.get(pattern) {
+        return *result;
     }
     if pattern.is_empty() {
         return 1;
     }
-
     for i in 1..=max_len.min(pattern.len()) {
         if valid.contains(&&pattern[..i]) {
-            let subcount = count_valid_pattern(&pattern[i..], valid, already_computed, max_len);
+            let subcount = count_valid_patterns(&pattern[i..], valid, already_computed, max_len);
             combinations += subcount;
         }
     }
@@ -41,7 +37,7 @@ fn count_valid_pattern<'a>(pattern: &'a str, valid: &Vec<&'a str>, already_compu
     combinations
 }
 
-fn parse_data<'a>(input: &'a str) -> (Vec<&'a str>, Vec<&'a str>) {
+fn parse_data(input: &str) -> (Vec<&str>, Vec<&str>) {
     let mut sections = input.split("\n\n");
     let valid = sections.next().unwrap().split(", ").collect();
     let designs = sections.next().unwrap().lines().collect();
@@ -66,12 +62,12 @@ bbrgwb";
     #[test]
     fn test_part1() {
         let (valid, designs) = parse_data(TESTDATA);
-        assert_eq!(part1(&designs, &valid), 6);
+        assert_eq!(solve(&designs, &valid).0, 6);
     }
 
     #[test]
     fn test_part2() {
         let (valid, designs) = parse_data(TESTDATA);
-        assert_eq!(part2(&designs, &valid), 16);
+        assert_eq!(solve(&designs, &valid).1, 16);
     }
 }

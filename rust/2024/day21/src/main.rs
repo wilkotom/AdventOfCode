43d1080f5759f18608b59b2fb47d2d5bd34bd56f
Tom Wilkinson <tom.github@dentrasssi.net>
2024-12-22 14:02:28 +0000

    Robocomplexity high. I couldn't give a monkey's.

--- rust/2024/day21/src/main.rs
+++ rust/2024/day21/src/main.rs
@@ -1,24 +1,207 @@
-use std::error::Error;
-use aochelpers::get_daily_input;
+use std::{collections::HashMap, error::Error};
+use aochelpers::{get_daily_input, Coordinate};
 
+ 
 fn main() -> Result<(), Box<dyn Error>>{
-    //let data = get_daily_input(21,2024)?;
-    
+    let data = get_daily_input(21,2024)?;
+    let sequences = data.lines().collect::<Vec<&str>>();
+    println!("{}", part1(&sequences));
+    println!("{}", part2(&sequences));
+
     Ok(())
 }
 
+fn part1(numbers: &[&str]) -> usize {
+    numbers.iter().map(|s| shortest_sequence(&first_stage(s),1) * s[0..3].parse::<usize>().unwrap()).sum()
+}
+
+fn part2(numbers: &[&str]) -> usize {
+    numbers.iter().map(|s: &&str| shortest_sequence(&first_stage(s),24) * s[0..3].parse::<usize>().unwrap()).sum()
+}
+
+fn shortest_sequence(sequence: &str, depth: usize) -> usize {
+    let direction_lookups: HashMap<char, HashMap<char, &str>> = HashMap::from([
+        ('A', HashMap::from([('A', "A"),    ('^', "<A"),  ('>', "vA"),  ('v', "<vA"), ('<', "v<<A")  ])),
+        ('^', HashMap::from([('A', ">A"),   ('^', "A"),   ('>', "v>A"), ('v', "vA"),  ('<', "v<A")  ])),
+        ('v', HashMap::from([('A', "^>A"),  ('^', "^A"),  ('>', ">A"),  ('v', "A"),   ('<', "<A")  ])),
+        ('<', HashMap::from([('A', ">>^A"), ('^', ">^A"), ('>', ">>A"), ('v', ">A"),  ('<', "A")  ])),
+        ('>', HashMap::from([('A', "^A"),   ('^', "<^A"), ('>', "A"),   ('v', "<A"),  ('<', "<<A")  ])),
+    ]);
+
+    let mut cache: HashMap<(char, char, usize), usize> = HashMap::new();
+    for start in ['A', '<', 'v', '>', '^'] {
+        for end in  ['A', '<', 'v', '>', '^'] {
+            cache.insert((start, end, 0), direction_lookups.get(&start).unwrap().get(&end).unwrap().len());
+        }
+    }
+    let mut current_button = 'A';
+    let mut result = 0;
+    for target in  sequence.chars() {
+        result+= shortest_instruction_length(current_button, target, depth, &mut cache, &direction_lookups);
+        current_button = target;
+    }
+    result
+}
+
+
+fn shortest_instruction_length(
+            start:char, 
+            end:char, 
+            level: usize, 
+            cache: &mut HashMap<(char, char, usize), usize>,
+            direction_lookups: &HashMap<char, HashMap<char, &str>>) -> usize {
+
+    if let Some(precomputed) = cache.get(&(start,end,level))  {
+        return *precomputed;
+    }
+    let target = direction_lookups.get(&start).unwrap()
+            .get(&end).unwrap();
+    let mut result = 0;
+    let mut current='A';
+    for next_char in target.chars() {
+        result += shortest_instruction_length(current, next_char, level-1, cache, direction_lookups);
+        current = next_char;
+    }
+    cache.insert((start,end,level), result);
+    result
+}
+
+fn first_stage(sequence: &str) -> String {
+    let direction_lookups: HashMap<char, HashMap<char, &str>> = HashMap::from([
+        ('A', HashMap::from([('A', ""), ('0', "<"),('1', "^<<"),('2', "<^"),('3', "^"),('4', "^^<<"),('5', "<^^"),('6', "^^"),('7', "^^^<<"),('8', "^^^<"), ('9', "^^^")])),
+        ('0', HashMap::from([('A', ">"), ('0', ""),('1', "^<"),('2', "^"),('3', ">^"),('4', "^^<"),('5', "^^"),('6', ">^^"),('7', "^^^<"),('8', "^^^"), ('9', ">^^^")])),
+        ('1', HashMap::from([('A', ">>v"), ('0', ">v"),('1', ""),('2', ">"),('3', ">>"),('4', "^"),('5', "^>"),('6', ">>^"),('7', "^^"),('8', "^^>"), ('9', "^^>>")])),
+        ('2', HashMap::from([('A', ">v"), ('0', "v"),('1', "<"),('2', ""),('3', ">"),('4', "^<"),('5', "^"),('6', ">^"),('7', "<^^"),('8', "^^"), ('9', ">^^")])),
+        ('3', HashMap::from([('A', "v"), ('0', "v<"),('1', "<<"),('2', "<"),('3', ""),('4', "^<<"),('5', "^<"),('6', "^"),('7', "<<^^"),('8', "^^<"), ('9', "^^")])),
+        ('4', HashMap::from([('A', ">>vv"), ('0', ">vv"),('1', "v"),('2', ">v"),('3', ">>v"),('4', ""),('5', ">"),('6', ">>"),('7', "^"),('8', ">^"), ('9', ">>^")])),
+        ('5', HashMap::from([('A', ">vv"), ('0', "vv"),('1', "v<"),('2', "v"),('3', ">v"),('4', "<"),('5', ""),('6', ">"),('7', "^<"),('8', "^"), ('9', "^>")])),
+        ('6', HashMap::from([('A', "vv"), ('0', "<vv"),('1', "<<v"),('2', "<v"),('3', "v"),('4', "<<"),('5', "<"),('6', ""),('7', "<<^"),('8', "^<"), ('9', "^")])),
+        ('7', HashMap::from([('A', ">>vvv"), ('0', ">vvv"),('1', "vv"),('2', "vv>"),('3', ">>vv"),('4', "v"),('5', ">v"),('6', ">>v"),('7', ""),('8', ">"), ('9', ">>")])),
+        ('8', HashMap::from([('A', ">vvv"), ('0', "vvv"),('1', "vv<"),('2', "vv"),('3', "vv>"),('4', "v<"),('5', "v"),('6', ">v"),('7', "<"),('8', ""), ('9', ">")])),
+        ('9', HashMap::from([('A', "vvv"), ('0', "vvv<"),('1', "v<<"),('2', "vv<"),('3', "vv"),('4', "<<v"),('5', "<v"), ('6', "v"),('7', "<<"),('8', "<"), ('9', "")])),
+    ]);
+    let mut current_button = 'A';
+    let mut result = String::new();
+    for target in  sequence.chars() {
+        result.push_str(direction_lookups.get(&current_button).unwrap().get(&target).unwrap());
+        result.push('A');
+        current_button = target;
+    }
+
+    result
+}
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part1() {
-        
+    fn validate_final_instructions(sequence: &str) -> String {
+        let keypad = HashMap::from([
+            (Coordinate{x:0,y:0}, '7'),
+            (Coordinate{x:1,y:0}, '8'),
+            (Coordinate{x:2,y:0}, '9'),
+            (Coordinate{x:0,y:1}, '4'),
+            (Coordinate{x:1,y:1}, '5'),
+            (Coordinate{x:2,y:1}, '6'),
+            (Coordinate{x:0,y:2}, '1'),
+            (Coordinate{x:1,y:2}, '2'),
+            (Coordinate{x:2,y:2}, '3'),
+            (Coordinate{x:1,y:3}, '0'),
+            (Coordinate{x:2,y:3}, 'A'),
+        ]);
+    
+        let mut pos = Coordinate{x:2, y:3};
+        let mut res = String::new();
+        for c in sequence.chars() {
+            match c {
+                '^' => {pos.y -=1},
+                'v' => {pos.y +=1}
+                '<' => {pos.x -=1}
+                '>' => {pos.x +=1}
+                'A' => {res.push(*keypad.get(&pos).unwrap()); }
+                _ => unimplemented!()
+            }
+        }
+        res
     }
+
+    fn validate_intermediate_instructions(sequence: &str) -> String {
+        let keypad = HashMap::from([
+            (Coordinate{x:1,y:0}, '^'),
+            (Coordinate{x:2,y:0}, 'A'),
+            (Coordinate{x:0,y:1}, '<'),
+            (Coordinate{x:1,y:1}, 'v'),
+            (Coordinate{x:2,y:1}, '>'),
+        ]);
+        let mut pos = Coordinate{x:2, y:0};
+        let mut res = String::new();
+        for c in sequence.chars() {
+            match c {
+                '^' => {pos.y -=1},
+                'v' => {pos.y +=1}
+                '<' => {pos.x -=1}
+                '>' => {pos.x +=1}
+                'A' => {res.push(*keypad.get(&pos).unwrap()); }
+                _ => unimplemented!()
+            }
+        }
+        res
+    }
+    
+    #[test]
+    fn test_part_1_029() {
+        let first_pass = first_stage("029A");
+        assert_eq!(validate_final_instructions(&first_pass), "029A");
+    }
+
+    #[test]
+    fn test_part_1_980() {
+        let first_pass = first_stage("980A");
+        assert_eq!(validate_final_instructions(&first_pass), "980A");
+    }
+
+
+    #[test]
+    fn test_part_1_179() {
+        let first_pass = first_stage("179A");
+        assert_eq!(validate_final_instructions(&first_pass), "179A");
+    }
+
+
+    #[test]
+    fn test_part_1_456() {
+        let first_pass = first_stage("456A");
+        assert_eq!(validate_final_instructions(&first_pass), "456A");
+    }
+
+
+    #[test]
+    fn test_part_1_379() {
+        let first_pass = first_stage("379A");
+        assert_eq!(validate_final_instructions(&first_pass), "379A");
+    }
+
+    #[test]
+    fn test_shortest_seq_p1() {
+        assert_eq!(shortest_sequence(&first_stage("029A"),1), 68);
+        assert_eq!(shortest_sequence(&first_stage("980A"),1), 60);
+        assert_eq!(shortest_sequence(&first_stage("179A"),1), 68);
+        assert_eq!(shortest_sequence(&first_stage("456A"),1), 64);
+        assert_eq!(shortest_sequence(&first_stage("379A"),1), 64);
+    }
+    #[test]
+    fn test_calculate_compound_complexity() {
+        assert_eq!(part1(&["029A", "980A", "179A", "456A", "379A"]), 126384);
+    }
+
 
     #[test]
     fn test_part2() {
     }
+
 }
+/*
+  left: "v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A"
+ right: "v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"
+      */
--- rust/2024/day22/src/main.rs
+++ rust/2024/day22/src/main.rs
@@ -1,13 +1,82 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 use aochelpers::get_daily_input;
 
 fn main() -> Result<(), Box<dyn Error>>{
-    //let data = get_daily_input(22,2024)?;
-    
+    let data = get_daily_input(22,2024)?;
+    let secrets = data.lines().map(|v| v.parse::<isize>().unwrap()).collect::<Vec<_>>();
+    //let secrets = [1,10,100,2024];
+    //let results = part1(&secrets);
+    // println!("{}", results.iter().sum::<usize>());
+
+    println!("{}", part2(&secrets));
     Ok(())
 }
 
+fn part1(secrets: &[isize]) -> Vec<isize> {
+    let mut answer = Vec::new();
+    for secret in secrets {
+        let mut secret = *secret;
+        (0..2000).for_each(|_| next_secret_number(&mut secret));
+        answer.push(secret)
+    }
+    answer
+}
 
+fn part2(secrets: &[isize]) -> isize {
+    let mut prices_by_monkey: Vec<Vec<isize>> = Vec::new();
+    let mut deltas_by_monkey: Vec<Vec<isize>> = Vec::new();
+    for secret in secrets {
+        let mut prices = Vec::new();
+        let mut deltas = Vec::new();
+        let mut secret = *secret;
+        prices.push(secret % 10);
+        for i in 1..=2000 {
+            next_secret_number(&mut secret);
+            prices.push(secret % 10);
+            deltas.push(secret % 10 - prices[i-1] );
+        }
+        prices_by_monkey.push(prices);
+        deltas_by_monkey.push(deltas);
+    }
+
+    let mut scores_for_sequence = HashMap::new();
+    let mut best_score = 0;
+    for monkey in deltas_by_monkey.iter() {
+        for window in monkey.windows(4) {
+            if scores_for_sequence.contains_key(window) {
+                continue;
+            }
+            let mut score_for_tuple = 0;
+            for (i, monkey) in deltas_by_monkey.iter().enumerate() {
+                for (j,comparator_window) in monkey.windows(4).enumerate() {
+                    if window == comparator_window {
+                        score_for_tuple += prices_by_monkey[i][j+4];
+                        break;
+                    }
+                }
+            }
+            if score_for_tuple > best_score {
+                println!("New best: {:?} {}", window, score_for_tuple);
+                best_score = score_for_tuple;
+            }
+            scores_for_sequence.insert(window, score_for_tuple);
+        }
+    }
+    *scores_for_sequence.values().max().unwrap()
+}
+
+fn next_secret_number(num: &mut isize) {
+    
+    *num ^= *num*64;
+    *num %= 16777216;
+
+    *num ^= *num/32;
+    *num %= 16777216;
+
+    *num ^= *num*2048;
+    *num %= 16777216;
+
+}
 
 #[cfg(test)]
 mod tests {
@@ -15,7 +84,8 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        
+        let secrets = [1,10,100,2024];
+        assert_eq!(part1(&secrets), vec![8685429,4700978,15273692, 8667524]);
     }
 
     #[test]

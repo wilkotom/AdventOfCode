f2b74267f16beb4da0e61cc3e1fb44db628fecbb
Tom Wilkinson <tom.github@dentrasssi.net>
2024-12-22 16:44:35 +0000

    Performance optimisation - faster

--- rust/2024/day22/src/main.rs
+++ rust/2024/day22/src/main.rs
@@ -1,31 +1,28 @@
-use std::{collections::HashMap, error::Error};
+use std::{collections::{HashMap, HashSet}, error::Error, slice::Windows};
 use aochelpers::get_daily_input;
 
 fn main() -> Result<(), Box<dyn Error>>{
     let data = get_daily_input(22,2024)?;
     let secrets = data.lines().map(|v| v.parse::<isize>().unwrap()).collect::<Vec<_>>();
-    //let secrets = [1,10,100,2024];
-    //let results = part1(&secrets);
-    // println!("{}", results.iter().sum::<usize>());
-
-    println!("{}", part2(&secrets));
+    println!("Part 1: {}", part1(&secrets));
+    println!("Part 2: {}", part2(&secrets));
     Ok(())
 }
 
-fn part1(secrets: &[isize]) -> Vec<isize> {
-    let mut answer = Vec::new();
+fn part1(secrets: &[isize]) -> isize {
+    let mut final_value = 0;
     for secret in secrets {
         let mut secret = *secret;
         (0..2000).for_each(|_| next_secret_number(&mut secret));
-        answer.push(secret)
+        final_value += secret
     }
-    answer
+    final_value
 }
 
 fn part2(secrets: &[isize]) -> isize {
-    let mut prices_by_monkey: Vec<Vec<isize>> = Vec::new();
-    let mut deltas_by_monkey: Vec<Vec<isize>> = Vec::new();
+    let mut sell_price_by_window= HashMap::new();
     for secret in secrets {
+        let mut seen = HashSet::new();
         let mut prices = Vec::new();
         let mut deltas = Vec::new();
         let mut secret = *secret;
@@ -35,44 +32,23 @@ fn part2(secrets: &[isize]) -> isize {
             prices.push(secret % 10);
             deltas.push(secret % 10 - prices[i-1] );
         }
-        prices_by_monkey.push(prices);
-        deltas_by_monkey.push(deltas);
-    }
-
-    let mut scores_for_sequence = HashMap::new();
-    let mut best_score = 0;
-    for monkey in deltas_by_monkey.iter() {
-        for window in monkey.windows(4) {
-            if scores_for_sequence.contains_key(window) {
-                continue;
+        for (i,window) in deltas.windows(4).enumerate()  {
+            if !seen.contains(&window) {
+                seen.insert(window);
+                *sell_price_by_window.entry([window[0], window[1], window[2], window[3]]).or_insert(0) += prices[i+4]
             }
-            let mut score_for_tuple = 0;
-            for (i, monkey) in deltas_by_monkey.iter().enumerate() {
-                for (j,comparator_window) in monkey.windows(4).enumerate() {
-                    if window == comparator_window {
-                        score_for_tuple += prices_by_monkey[i][j+4];
-                        break;
-                    }
-                }
-            }
-            if score_for_tuple > best_score {
-                println!("New best: {:?} {}", window, score_for_tuple);
-                best_score = score_for_tuple;
-            }
-            scores_for_sequence.insert(window, score_for_tuple);
         }
     }
-    *scores_for_sequence.values().max().unwrap()
+    *sell_price_by_window.values().max().unwrap()
 }
+
 
 fn next_secret_number(num: &mut isize) {
     
     *num ^= *num*64;
     *num %= 16777216;
-
     *num ^= *num/32;
     *num %= 16777216;
-
     *num ^= *num*2048;
     *num %= 16777216;
 
